d752a241f1c5567c8fdc612d0661c05ecc082efb
JKBoy2244 <jobayer2006@gmail.com>
2026-03-07 05:14:45 +0000

    Create IStudentInfo.cs

---
+++ src/LabMarkingQueueTracker/Interfaces/IStudentInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace myApplication;
+
+interface IStudentInfo 
+{
+
+  String getFullName();
+  int getseatNumber();
+}
